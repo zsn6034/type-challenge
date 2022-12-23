3ad91591849e8e3382b5e3c9e152bf0b8f782ebd
zhuangshunan <zhuangshunan@baidu.com>
2022-12-23 18:40:43 +0800

    flatten

---
+++ 00459-medium-flatten.ts
@@ -0,0 +1,32 @@
+/*
+ * @Descripttion:
+ * @Author: zhuangshunan
+ * @Date: 2022-12-23 18:32:11
+ * @LastEditors: zhuangshunan
+ * @LastEditTime: 2022-12-23 18:40:27
+ */
+// ============= Test Cases =============
+import type { Equal, Expect } from './test-utils';
+
+type cases = [
+  Expect<Equal<Flatten<[]>, []>>,
+  Expect<Equal<Flatten<[1, 2, 3, 4]>, [1, 2, 3, 4]>>,
+  Expect<Equal<Flatten<[1, [2]]>, [1, 2]>>,
+  Expect<Equal<Flatten<[1, 2, [3, 4], [[[5]]]]>, [1, 2, 3, 4, 5]>>,
+  Expect<
+    Equal<
+      Flatten<[{ foo: 'bar'; 2: 10 }, 'foobar']>,
+      [{ foo: 'bar'; 2: 10 }, 'foobar']
+    >
+  >
+];
+
+// ============= Your Code Here =============
+type Flatten<T extends any[], Result extends any[] = []> = T extends [
+  infer Start,
+  ...infer Rest
+]
+  ? Start extends any[]
+    ? Flatten<Rest, [...Result, ...Flatten<Start>]>
+    : Flatten<Rest, [...Result, Start]>
+  : Result;

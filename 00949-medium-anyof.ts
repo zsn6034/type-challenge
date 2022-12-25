43053269d15e8f26231b16a5afcf0f40b35c8037
zsn6034 <826776415@qq.com>
2022-12-25 22:40:30 +0800

    anyof

---
+++ 00949-medium-anyof.ts
@@ -0,0 +1,28 @@
+/*
+ * @Description: 
+ * @Author: zhuangshunan
+ * @Date: 2022-12-25 22:19:43
+ * @LastEditors: zhuangshunan
+ * @LastEditTime: 2022-12-25 22:38:53
+ */
+// ============= Test Cases =============
+import type { Equal, Expect } from './test-utils'
+
+type cases = [
+  Expect<Equal<AnyOf<[1, 'test', true, [1], { name: 'test' }, { 1: 'test' }]>, true>>,
+  Expect<Equal<AnyOf<[1, '', false, [], {}]>, true>>,
+  Expect<Equal<AnyOf<[0, 'test', false, [], {}]>, true>>,
+  Expect<Equal<AnyOf<[0, '', true, [], {}]>, true>>,
+  Expect<Equal<AnyOf<[0, '', false, [1], {}]>, true>>,
+  Expect<Equal<AnyOf<[0, '', false, [], { name: 'test' }]>, true>>,
+  Expect<Equal<AnyOf<[0, '', false, [], { 1: 'test' }]>, true>>,
+  Expect<Equal<AnyOf<[0, '', false, [], { name: 'test' }, { 1: 'test' }]>, true>>,
+  Expect<Equal<AnyOf<[0, '', false, [], {}, undefined, null]>, false>>,
+  Expect<Equal<AnyOf<[]>, false>>,
+]
+
+
+// ============= Your Code Here =============
+type Falsy = 0 | "" | undefined | null | false | [] | Record<PropertyKey, never>;
+type AnyOf<T extends readonly any[]> = T[number] extends Falsy ? false : true;
+

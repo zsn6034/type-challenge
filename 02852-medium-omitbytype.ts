5546e0d13d45b223304f997a95f7581ac08a6418
zsn6034 <826776415@qq.com>
2023-01-23 11:15:17 +0800

    omit-by-type

---
+++ 02852-medium-omitbytype.ts
@@ -0,0 +1,28 @@
+/*
+ * @Description: 
+ * @Author: zhuangshunan
+ * @Date: 2023-01-23 11:14:02
+ * @LastEditors: zhuangshunan
+ * @LastEditTime: 2023-01-23 11:15:04
+ */
+// ============= Test Cases =============
+import type { Equal, Expect } from './test-utils'
+
+interface Model {
+  name: string
+  count: number
+  isReadonly: boolean
+  isEnable: boolean
+}
+
+type cases = [
+  Expect<Equal<OmitByType<Model, boolean>, { name: string; count: number }>>,
+  Expect<Equal<OmitByType<Model, string>, { count: number; isReadonly: boolean; isEnable: boolean }>>,
+  Expect<Equal<OmitByType<Model, number>, { name: string; isReadonly: boolean; isEnable: boolean }>>,
+]
+
+
+// ============= Your Code Here =============
+type OmitByType<T, U> = {
+  [K in keyof T as T[K] extends U ? never : K]: T[K];
+}

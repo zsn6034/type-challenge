2aa76fb392ec3cae5a12ee5fcff0f93e04b5efc1
zsn6034 <826776415@qq.com>
2023-01-24 22:37:37 +0800

    reverse

---
+++ 03192-medium-reverse.ts
@@ -0,0 +1,27 @@
+/*
+ * @Description: 
+ * @Author: zhuangshunan
+ * @Date: 2023-01-24 22:32:21
+ * @LastEditors: zhuangshunan
+ * @LastEditTime: 2023-01-24 22:36:44
+ */
+// ============= Test Cases =============
+import type { Equal, Expect } from './test-utils'
+
+type cases = [
+  Expect<Equal<Reverse<[]>, []>>,
+  Expect<Equal<Reverse<['a', 'b']>, ['b', 'a']>>,
+  Expect<Equal<Reverse<['a', 'b', 'c']>, ['c', 'b', 'a']>>,
+]
+
+type errors = [
+  // @ts-expect-error
+  Reverse<'string'>,
+  // @ts-expect-error
+  Reverse<{ key: 'value' }>,
+]
+
+
+// ============= Your Code Here =============
+type Reverse<T extends unknown[]> = T extends [infer X, ...infer Rest]
+  ? [...Reverse<Rest>, X] : T;
